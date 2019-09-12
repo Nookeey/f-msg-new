310ef90516b1878fec112f9398ee97bcb7d32c84
nookeey <eernestt@o2.pl>
2019-09-12 16:28:56 +0200

    auth form

--- lib/models/user.dart
+++ lib/models/user.dart
@@ -2,12 +2,16 @@ import 'package:flutter/material.dart';
 
 class User {
   final String id;
+  final String email;
+  final String password;
   final String avatar;
   final String nick;
 
   User({
-    @required this.id,
-    @required this.avatar,
-    @required this.nick,
+    this.id,
+    @required this.email,
+    this.password,
+    this.avatar,
+    this.nick,
   });
 }
--- lib/screens/auth_screen.dart
+++ lib/screens/auth_screen.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
 
+import '../models/user.dart';
+
 class AuthScreen extends StatefulWidget {
   static final routeName = 'auth';
 
@@ -10,8 +12,71 @@ class AuthScreen extends StatefulWidget {
 }
 
 class _AuthScreenState extends State<AuthScreen> {
+  final _passwordFocusNode = FocusNode();
+  final _form = GlobalKey<FormState>();
+  var _user = User(
+    id: null,
+    email: '',
+    password: '',
+    avatar: '',
+    nick: ''
+  );
+
+  void _saveForm() {
+    _form.currentState.save();
+    print(_user.email);
+    print(_user.password);
+  }
+
   @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Auth'),
+        actions: <Widget>[
+          IconButton(
+            icon: Icon(Icons.save),
+            onPressed: _saveForm,
+          )
+        ],
+      ),
+      body: Padding(
+        padding: const EdgeInsets.all(16.0),
+        child: Form(
+          key: _form,
+          child: ListView(
+            children: <Widget>[
+              TextFormField(
+                decoration: InputDecoration(labelText: 'Email'),
+                textInputAction: TextInputAction.next,
+                keyboardType: TextInputType.emailAddress,
+                onFieldSubmitted: (_) {
+                  FocusScope.of(context).requestFocus(_passwordFocusNode);
+                },
+                onSaved: (value) {
+                  _user = User(
+                    email: value,
+                    password: _user.password
+                  );
+                },
+              ),
+              TextFormField(
+                decoration: InputDecoration(labelText: 'Password'),
+                textInputAction: TextInputAction.next,
+                keyboardType: TextInputType.text,
+                focusNode: _passwordFocusNode,
+                obscureText: true,
+                onSaved: (value) {
+                  _user = User(
+                    email: _user.email,
+                    password: value
+                  );
+                },
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
   }
 }
